ea5ceb00d30ec38c211689813d5400f168a77da8
Md Azad <mo.azad1999@gmail.com>
2023-07-07 21:13:55 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,44 @@
 import Image from 'next/image'
+import { Metadata } from 'next';
+
+
+export const metadata:Metadata = {
+  title: 'Web3 Hack Statistics, Hackerboard - QuillAudits',
+  description:
+    'Get statistics and deep analysis of recent crypto hacks, vulnerabilities, and attack vectors around the web3 world.',
+  openGraph: {
+    title: 'Web3 Hack Statistics, Hackerboard - QuillAudits',
+    url: 'https://www.quillaudits.com/tools/hackerboard',
+    description:
+      'Get statistics and deep analysis of recent crypto hacks, vulnerabilities, and attack vectors around the web3 world.',
+    images: [
+      {
+        url: 'https://quillaudits.com/api/og?amount=6B&number=1000',
+        secureUrl: 'https://quillaudits.com/api/og?amount=6B&number=1000',
+        alt: 'Web3 Hack Statistics, Hackerboard - QuillAudits',
+        width: 1200,
+        height: 630,
+        type: 'image/png',
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  alternates: {
+    canonical: 'https://www.quillaudits.com/tools/hackerboard',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Web3 Hack Statistics, Hackerboard - QuillAudits',
+    description:
+      'Get statistics and deep analysis of recent crypto hacks, vulnerabilities, and attack vectors around the web3 world.',
+    creator: '@QuillAudits',
+    images: ['https://quillaudits.com/api/og?amount=6B&number=1000'],
+  },
+  robots: {
+    index: true,
+  },
+};
 
 export default function Home() {
   return (
